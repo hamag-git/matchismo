ba3747856351a65c1107d0e3f1976d5e684d0efe
Kai Pervoelz <kpervoelz@macmini-art-1.iais.fraunhofer.de>
2014-03-14 14:16:08 +0100

    initial

---
+++ CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  matchismo
+//
+//  Created by Kai Pervölz on 28.02.14.
+//  Copyright (c) 2014 kpervoelz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  matchismo
+//
+//  Created by Kai Pervölz on 28.02.14.
+//  Copyright (c) 2014 kpervoelz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
