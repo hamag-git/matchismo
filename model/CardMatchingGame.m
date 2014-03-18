e0213342a8575956c1b1827b61d04d05401a5c9b
Kai Pervoelz <kpervoelz@macmini-art-1.iais.fraunhofer.de>
2014-03-18 14:31:37 +0100

    implemented version from lecture 3

--- model/CardMatchingGame.m
+++ model/CardMatchingGame.m
@@ -24,8 +24,18 @@
 }
 
 - (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck{
-    if(!self){
-        
+    self = [super init];
+    
+    if(self){
+        for (int i=0; i<count; i++) {
+            Card *card = [deck drawRandomCard];
+            if(card){
+                [self.cards addObject:card];
+            }else {
+                self = nil;
+                break;
+            }
+        }
         
         
     }
@@ -35,5 +45,52 @@
 - (instancetype)init{
     return nil;
 }
+
+-(Card *)cardAtIndex:(NSUInteger) index{
+    return (index<[self.cards count] ? self.cards[index] : nil);
+}
+
+static const int MATCH_PENALTY=2;
+static const int COST_TO_CHOOSE=1;
+static const int MATCH_BONUS=4;
+
+
+-(void)chooseCardAtIndex:(NSUInteger) index{
+    Card *card = [self cardAtIndex:index];
+    
+    if(!card.isMatched){
+        if(card.isChosen){
+            card.chosen= NO;
+        }else {
+            for(Card *otherCard in self.cards){
+                if(otherCard.isChosen && !otherCard.isMatched){
+                    int matchScore = [card match:@[otherCard]];
+                    if(matchScore){
+                        self.score += matchScore * MATCH_BONUS;
+                        otherCard.matched=YES;
+                        card.matched=YES;
+                    }else {
+                        self.score -= MATCH_PENALTY;
+                        otherCard.chosen=NO;
+                    }
+                    break;
+                    
+                }
+            }
+            self.score -= COST_TO_CHOOSE;
+            card.chosen= YES;
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+
 
 @end
